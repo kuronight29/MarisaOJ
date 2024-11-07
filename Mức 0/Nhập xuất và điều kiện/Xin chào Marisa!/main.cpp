39411fee863b384164be4ffe308f3583ec7a9937
kuronight29 <herryvu.dev@gmail.com>
2024-11-07 19:38:03 +0700

    up

---
+++ Mức 0/Nhập xuất và điều kiện/Xin chào Marisa!/main.cpp
@@ -0,0 +1,15 @@
+/*
+    @author longvuuuu
+    @github kuronight29
+*/
+#include <bits/stdc++.h>
+#define taskname ""
+#define ll long long
+#define pb push_back
+using namespace std;
+int main() {
+    //freopen(taskname ".inp", "r", stdin);
+    //freopen(taskname ".out", "w", stdout);
+    cout << "Hello Marisa!";
+    return 0;
+}
