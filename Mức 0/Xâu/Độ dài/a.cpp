aaba91c332afce8fa71eeabca5dda065f801346b
kuronight29 <herryvu.dev@gmail.com>
2024-11-12 20:06:12 +0700

    up

---
+++ Mức 0/Xâu/Độ dài/a.cpp
@@ -0,0 +1,20 @@
+/*
+    @author longvuuuu
+    @github kuronight29
+*/
+#include <bits/stdc++.h>
+#define taskname ""
+#define ll long long
+#define pb push_back
+using namespace std;
+
+int main() {
+    if(fopen((string(taskname) + ".inp").c_str(), "r") != NULL) {
+        freopen((string(taskname) + ".inp").c_str(), "r", stdin);
+        freopen((string(taskname) + ".out").c_str(), "w", stdout);
+    }
+    string st;
+    getline(cin, st);
+    cout << st.size();
+    return 0;
+}
